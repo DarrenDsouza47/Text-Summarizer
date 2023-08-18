adb162c2f8d407b48e5fc242d1cfbaeb3326e6f1
darrendsouza <darrendsouza66@gmail.com>
2023-08-18 17:57:31 +0530

    folder name updated

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 
+with open("README.MD","r",encoding="utf-8") as f:
+    long_description=f.read()
 __version__="0.0.0"
 
 REPO_NAME="Text-Summarizer"
 AUTHOR_USER_NAME="DarrenDsouza47"
-SRC_REPO="text-Summarizer"
+SRC_REPO="textSummarizer"
 AUTHOR_EMAIL="darrendsouza66@gmail.com"
 
 setuptools.setup(
@@ -12,6 +14,7 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
+    long_description=long_description,
     description="a small python package for NLP app",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
