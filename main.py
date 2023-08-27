f4705df034fc449d7518751397abc132ace1e20e
darrendsouza <darrendsouza66@gmail.com>
2023-08-27 16:19:31 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME="Stage 01 data ingestion"
@@ -7,6 +8,18 @@ try:
     logger.info(f">>>>>stage {STAGE_NAME}<<<<<<")
     data_ingestion=DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>stage {STAGE_NAME} completed<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Stage 02 data validation"
+
+try:
+    logger.info(f">>>>>stage {STAGE_NAME}<<<<<<")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>stage {STAGE_NAME} completed<<<<<")
 except Exception as e:
     logger.exception(e)
