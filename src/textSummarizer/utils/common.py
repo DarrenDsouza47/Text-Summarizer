800409c9665cb3799b58aeff502af4942fc67a55
darrendsouza <darrendsouza66@gmail.com>
2023-08-27 07:52:40 +0530

    download and unzip

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -5,7 +5,7 @@ from textSummarizer.logging import logger
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path 
-from typing import any
+from typing import Any
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
