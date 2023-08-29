cba4d0422488ab91e3ae482b0ba13a5f368ed1af
darrendsouza <darrendsouza66@gmail.com>
2023-08-29 17:49:58 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME="Stage 01 data ingestion"
@@ -32,6 +34,28 @@ try:
     logger.info(f">>>>>stage {STAGE_NAME}<<<<<<")
     data_transformation=DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>stage {STAGE_NAME} completed<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Stage 04 Model Trainer"
+ 
+try:
+    logger.info(f">>>>>stage {STAGE_NAME}<<<<<<")
+    model_trainer=ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>stage {STAGE_NAME} completed<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Stage 05 Model Evaluation"
+ 
+try:
+    logger.info(f">>>>>stage {STAGE_NAME}<<<<<<")
+    model_evaluation=ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>stage {STAGE_NAME} completed<<<<<")
 except Exception as e:
     logger.exception(e)
